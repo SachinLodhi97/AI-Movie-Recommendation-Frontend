a9a07ffc1e8ddceac5615d00cd5952f1264389fa
SachinLodhi97 <sachinlodhi058@gmail.com>
2026-04-14 15:53:01 +0530

    fix

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,7 @@
 function MovieCard({ movie, onClick }) {
   return (
     <div className="card" onClick={onClick}>
-      <img src={movie.poster} />
+      <img src={movie.poster} alt={movie.title} />
       <p>{movie.title}</p>
     </div>
   );
